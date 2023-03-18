30fe7ea5e53365fd91aef9ce29a3e4b13ec4fd69
Ferry Djaja <ferrygun@yahoo.com>
2023-03-18 14:25:23 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import network
+import time
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("", "")
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("Waiting to connect:")
+    time.sleep(1)
+    
+    
+from urlencode import urlencode
+from pichromecast import play_url
+
+url = 'https://translate.google.com/translate_tts?client=tw-ob&' + urlencode({'q': 'Hello, 世界', 'tl': 'ja'})
+play_url(url, '192.168.50.23')
